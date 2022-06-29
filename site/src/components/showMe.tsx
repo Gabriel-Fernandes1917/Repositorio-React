4d802f3e7c93855c00616256ad24958288faacad
Gabriel Fernandes <gabgui2001@gmail.com>
2022-06-29 00:30:55 -0300

    .

--- site/src/components/showMe.tsx
+++ site/src/components/showMe.tsx
@@ -4,17 +4,18 @@ export function ShowMe() {
     
     return(
         <div className="w-full h-[90vh] bg-bgDark text-white">
-            <div className="">
-                <div>
-                    <div>
-                    <h1
-                    className="">Gabriel Fernandes</h1>
-                    <p>
+            <div className="w-[80%] pt-[9rem]">
+                <div className="flex w-full h-full">
+                    <div className="mx-auto my-auto">
+                    <h1 className="text-6xl">
+                        Gabriel Fernandes
+                    </h1>
+                    <p className="text-2xl w-min">
                         Ola sou programador Web Front-end JS, sou apaixonado por empreendedorismo, inovações e desafios principalemnte
                     </p>
                     </div>
 
-                    <img src="" alt="PictureMe" />
+                    <img src="./src/assets/protfiINM.jpg" alt="PictureMe" className="w-[400px] h-[400px] rounded-[50%]"/>
                 </div>
             </div>
         </div>
